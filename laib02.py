f85b7633ecbdf42a8370c085c14d68b48ac5ab81
enzo <s273852@studenti.polito.it>
2023-11-20 12:21:31 +0100

    commit es2 laib2 there are two problem in es1 and es4

--- laib02.py
+++ laib02.py
@@ -6,6 +6,8 @@
 #plot,scatter,bar,hist,  f,axs=plt.subplots()
 import pandas as pd
 import matplotlib.pyplot as plt
+import json5 as js
+
 df=pd.read_csv('sales_data.csv')
 df.head()
 profit_list=df['total_profit'].values
@@ -98,3 +100,73 @@ plt . show()
 #ES2
 #lettura file JSON
 
+#1 convert a json_obj in a python obj
+
+json_obj='{"Name":"David","Class"="I","Age":6}'
+python_obj=js.load(json_obj)
+print('\n Name > ', python_obj['Name'])
+print('\nClass > ', python_obj['Class'])
+print('\nAge > ',python_obj['Age'])
+
+#2 convert a python obj in a json data
+
+python_ob={
+   'name':'David',
+    'class':'i',
+    'age': 6
+}
+j_data=js.dumps(python_ob)
+print(type(python_ob))
+print('\nJson data : ', j_data)
+
+#3 convert python objs in a JSON strings
+
+python_dict = { 'name': 'David' , 'age' : 6 , 'class': 'I'}
+python_list = [ 'Red' , 'Green' , 'Black']
+python_str = 'Python Json'
+python_int = 1234
+python_float = 21.34
+python_t = True
+python_f = False
+python_n = None
+json_dict=js.dumps(python_dict)
+json_list=js.dumps(python_list)
+json_str=js.dumps(python_str)
+json_num1=js.dumps(python_int)
+json_num2=js.dumps(python_float)
+json_t=js.dumps(python_t)
+json_f=js.dumps(python_f)
+json_n=js.dumps(python_n)
+
+print ( 'json dict :' , json_dict )
+print ( 'jason list :' , json_list )
+print ( 'json string :' , json_str )
+print ( 'json number1 :' , json_num1 )
+print ( 'json number2 : ' , json_num2 )
+print ( 'json true : ', json_t )
+print ( 'json false : ' , json_f )
+print ( 'json null : ' , json_n )
+
+#4 convert Python dictonary to Json data
+# sort by key and print the object members with indent level 4
+
+j_str = {'4':5,'6':7,'1':3,'2':4}
+print('Original String ',j_str)
+print('\nJson data: ')
+print(js.dumps(j_str,sort_key=True, indent=4))
+
+#5 write a python programm to create a json file from an existing Json file
+
+
+with open('states.json') as f:
+    state_data=js.load()
+    print('\nOriginal JSON keys: ',
+          [state.keys() for state in state_data['states']][0])
+for state in state_data['states'] :
+    del state['area_codes']
+with open('new_state.json','w') as f:
+    js.dumps(state_data,f,indent=2)
+with open('new_state.json') as f:
+    state_data=js.load(f)
+print('\nReload JSON keys', [state.keys()
+      for state in state_data['states']][0])
